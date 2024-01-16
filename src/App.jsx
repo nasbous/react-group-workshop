9b708c7c5bc1276619de25b20b071f6166ed0552
espino vincent <tantalas95@gmail.com>
2024-01-16 11:06:47 +0100

    almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import ImageSlider from './ImageSlider';
+import audioFile from './assets/toasty_tfCWsU6.mp3';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [showSlider, setShowSlider] = useState(false);
+  const [audio] = useState(new Audio(audioFile));
+
+  const handleButtonClick = () => {
+    setShowSlider(true);
+    audio.play();
+  };
+
+  const handleAnimationEnd = () => {
+    setTimeout(() => {
+      setShowSlider(false);
+    }, 1000); 
+  };
+
+  useEffect(() => {
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, [audio]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={handleButtonClick} >
+        TOASTY !!
+      </button>
+      {showSlider && (
+        <ImageSlider
+          imageUrl="./src/assets/pngwing.com.png"
+          onAnimationEnd={handleAnimationEnd}
+        />
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
